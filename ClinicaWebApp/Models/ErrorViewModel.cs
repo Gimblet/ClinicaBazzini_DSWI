e6c5dfa3b6b9aec1f08c4e31fb26063ea2ac907e
Diego <gimbletdev@gmail.com>
2025-04-25 10:53:57 -0500

    Added Blazor Project

---
+++ ClinicaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClinicaWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
