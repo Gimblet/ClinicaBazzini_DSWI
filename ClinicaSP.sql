1f20364b85bfc126edaac0a4c9ef358e591ab460
Diego <gimbletdev@gmail.com>
2025-04-28 18:37:04 -0500

    Reordar Script SP

--- ClinicaSP.sql
+++ ClinicaSP.sql
@@ -1,65 +1,6 @@
 USE DSWI_Clinica
 GO
 
----------------------- CITA ------------------------
--- sp_columns cita
-
--- Lista todas las citas para el BackEnd
-CREATE OR ALTER PROC sp_listarCitasBack
-AS
-BEGIN
-    SELECT c.ide_cit,
-           c.cal_cit,
-           c.con_cit,
-           c.ide_med,
-           c.ide_pac,
-           c.ide_pag
-    FROM cita AS c
-END
-GO
-
--- Lista todas las citas para el FrontEnd
-CREATE OR ALTER PROC sp_listarCitasFront
-AS
-BEGIN
-    SELECT c.ide_cit,
-           c.cal_cit,
-           c.con_cit,
-           CONCAT(um.nom_usr, SPACE(1), um.ape_usr),
-           CONCAT(up.nom_usr, SPACE(1), up.ape_usr),
-           pg.mon_pag
-    FROM cita AS c
-             JOIN medico m ON m.ide_med = c.ide_med
-             JOIN usuario um ON um.ide_usr = m.ide_usr
-             JOIN paciente p ON p.ide_pac = c.ide_pac
-             JOIN usuario up ON up.ide_usr = p.ide_pac
-             JOIN pago pg ON c.ide_pag = pg.ide_pag
-END
-GO
-
--- Agrega Cita
-create or alter procedure sp_agregarCita(
-    @calendario DATETIME,
-    @consultorio INT,
-    @medico BIGINT,
-    @paciente BIGINT,
-    @pago BIGINT
-)
-AS
-BEGIN
-    INSERT INTO cita (cal_cit, con_cit, ide_med, ide_pac, ide_pag)
-    values (@calendario, @consultorio, @medico,
-            (SELECT p.ide_pac
-             FROM paciente p
-             WHERE p.ide_usr = @paciente), @pago)
-END
-GO
-
-        sp_agregarCita '2025-04-28 13:00', 2,
-        1, 1,
-        1
-GO
-
 -- Agrega Paciente
 CREATE OR ALTER PROC sp_agregarPaciente(
     @cor varchar(100),
@@ -197,4 +138,63 @@ END
 GO
 
 sp_obtenerIdUsuario 'joseph@gmail.com'
+GO
+
+---------------------- CITA ------------------------
+-- sp_columns cita
+
+-- Lista todas las citas para el BackEnd
+CREATE OR ALTER PROC sp_listarCitasBack
+AS
+BEGIN
+    SELECT c.ide_cit,
+           c.cal_cit,
+           c.con_cit,
+           c.ide_med,
+           c.ide_pac,
+           c.ide_pag
+    FROM cita AS c
+END
+GO
+
+-- Lista todas las citas para el FrontEnd
+CREATE OR ALTER PROC sp_listarCitasFront
+AS
+BEGIN
+    SELECT c.ide_cit,
+           c.cal_cit,
+           c.con_cit,
+           CONCAT(um.nom_usr, SPACE(1), um.ape_usr),
+           CONCAT(up.nom_usr, SPACE(1), up.ape_usr),
+           pg.mon_pag
+    FROM cita AS c
+             JOIN medico m ON m.ide_med = c.ide_med
+             JOIN usuario um ON um.ide_usr = m.ide_usr
+             JOIN paciente p ON p.ide_pac = c.ide_pac
+             JOIN usuario up ON up.ide_usr = p.ide_pac
+             JOIN pago pg ON c.ide_pag = pg.ide_pag
+END
+GO
+
+-- Agrega Cita
+create or alter procedure sp_agregarCita(
+    @calendario DATETIME,
+    @consultorio INT,
+    @medico BIGINT,
+    @paciente BIGINT,
+    @pago BIGINT
+)
+AS
+BEGIN
+    INSERT INTO cita (cal_cit, con_cit, ide_med, ide_pac, ide_pag)
+    values (@calendario, @consultorio, @medico,
+            (SELECT p.ide_pac
+             FROM paciente p
+             WHERE p.ide_usr = @paciente), @pago)
+END
+GO
+
+        sp_agregarCita '2025-04-28 13:00', 2,
+        1, 1,
+        1
 GO
