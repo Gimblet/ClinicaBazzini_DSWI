c53da4f32de854089800b19ec20c80736f09cf14
unknown <SUITE@SUITEA103-ST07.cibertec.edu.pe>
2025-05-03 16:31:32 -0500

    Recepcionists Views were added with relations to medic

--- ClinicaWebApp/Models/Usuario/Medico/MedicoO.cs
+++ ClinicaWebApp/Models/Usuario/Medico/MedicoO.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace ClinicaWebApp.Models.Usuario.Medico;
 
 public class MedicoO : UsuarioO
 {
+    [DisplayName("ID")]
     public long ide_med { get; set; }
+    [DisplayName("Sueldo")]
     public decimal sue_med { get; set; }
     public long ide_esp { get; set; }
     public long ide_usr { get; set; }
--- ClinicaWebApp/Models/Usuario/UsuarioO.cs
+++ ClinicaWebApp/Models/Usuario/UsuarioO.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel;
+
 namespace ClinicaWebApp.Models.Usuario;
 
 public class UsuarioO
 {
+    [DisplayName("Correo")]
     public string? cor_usr { get; set; }
+    [DisplayName("Contraseña")]
     public string? pwd_usr { get; set; }
+    [DisplayName("Nombre")]
     public string? nom_usr { get; set; }
+    [DisplayName("Apellido")]
     public string? ape_usr { get; set; }
+    [DisplayName("Fecha Nac.")]
     public DateTime fna_usr { get; set; }
-    public string num_doc { get; set; }
+    [DisplayName("Número de Doc.")]
+    public string? num_doc { get; set; }
     public long ide_doc { get; set; }
     public long ide_rol { get; set; }
 }
