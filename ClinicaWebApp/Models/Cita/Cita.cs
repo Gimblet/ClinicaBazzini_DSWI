5a8c5b974a8dd055752e794f59c8944e45a4860c
Diego <gimbletdev@gmail.com>
2025-05-10 10:25:10 -0500

    Various Aesthetic Improvements

--- ClinicaWebApp/Models/Cita/Cita.cs
+++ ClinicaWebApp/Models/Cita/Cita.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel;
+
 namespace ClinicaWebApp.Models.Cita;
 
 public class Cita
 {
+    [DisplayName("ID")]
     public long IdCita { get; set; }
+
+    [DisplayName("Fecha")]
     public DateTime CalendarioCita { get; set; }
+
+    [DisplayName("Nro. Consultorio")]
     public long Consultorio { get; set; }
+
+    [DisplayName("Médico")]
     public string? NombreMedico { get; set; }
+
+    [DisplayName("Paciente")]
     public string? NombrePaciente { get; set; }
+
+    [DisplayName("Precio")]
     public decimal MontoPago { get; set; }
 }
